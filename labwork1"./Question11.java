76e12c9e3b8620f96cf9b585b0e9c2ecc2e02f4c
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-10-16 15:44:18 +0300

    Add files via upload

---
+++ labwork1"./Question11.java
@@ -0,0 +1,17 @@
+
+package question1.pkg1;
+
+public class Question11 {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("NAME : AHMAD SHAJA AZIMI");
+        System.out.println("DEPARTMENT : COMPUTER ENGINERING");
+        System.out.println("STUDENT NUMBER : 6422003");
+       
+    }
+    
+}
