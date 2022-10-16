ec7a6ef8c59e2810d49eaebd310cd8bd3a685658
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-10-16 16:06:20 +0300

    Add files via upload

---
+++ LabWork2/Question3.java
@@ -0,0 +1,24 @@
+
+package question3;
+import java.util.Scanner;
+public class Question3 {
+
+    public static void main(String[] args) {
+   
+   Scanner input = new Scanner(System.in);
+   
+   System.out.println("Please enter three numbers:");
+   int a = input.nextInt();
+    System.out.println("2nd: ");
+   int b = input.nextInt();
+  System.out.println("3rd: ");
+   int c = input.nextInt();
+   
+        
+   
+   
+   System.out.println("Your numbers are "+a+ "  " + "   " + b  + "    "+ c);
+        
+    }
+    
+}
