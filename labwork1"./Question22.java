430b01e044609b8146e9a8f020c733709219d27b
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-10-16 15:48:23 +0300

    Add files via upload

---
+++ labwork1"./Question22.java
@@ -0,0 +1,18 @@
+
+package question1.pkg1;
+
+public class Question11 {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("*******");
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("*******");
+       
+    }
+    
+}
