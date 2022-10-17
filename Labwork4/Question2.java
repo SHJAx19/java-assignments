4242eef9e63bc704a952bcf27ee8fa9fad972903
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-10-17 18:35:22 +0300

    Add files via upload

---
+++ Labwork4/Question2.java
@@ -0,0 +1,18 @@
+
+package question2;
+
+public class Question2 {
+
+    public static void main(String[] args) {
+        
+        boolean isPositive = (-1 > 0);
+        if (isPositive)
+        System.out.println("The number is positive."); else
+        System.out.println("The number is negative or zero."); 
+        
+    
+        
+        
+    }
+    
+}
