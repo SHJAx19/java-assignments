336ce95b448077e7a9a8c520393754d0940f1b05
SHJA AZIMI <110715621+SHJAx19@users.noreply.github.com>
2022-12-25 17:42:25 +0300

    Update Question2.java

--- Labwork4/Question2.java
+++ Labwork4/Question2.java
@@ -6,11 +6,13 @@ public class Question2 {
     public static void main(String[] args) {
         
         boolean isPositive = (-1 > 0);
-        if (isPositive)
-        System.out.println("The number is positive."); else
+        if (isPositive){
+        System.out.println("The number is positive.");
+        }
+        else{
         System.out.println("The number is negative or zero."); 
         
-    
+        }
         
         
     }
